fb0234eed802f5d449a7f1caa129d51195cd3a5a
subashpernandas <subashpstalin@gmail.com>
2025-07-25 11:39:35 +0530

    second commit

---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+	private String name;
+	private int age;
+
+	
+	
+}
